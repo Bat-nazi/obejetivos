b9c9edb407da9c684b62a348ca8debe1dbd902ff
guilherme <128057990+Bat-nazi@users.noreply.github.com>
2024-04-29 11:48:06 -0300

    Add files via upload

---
+++ obejetivos-main/A1/main.js
@@ -0,0 +1,22 @@
+const botoes = document.querySelectorAll(".botao");
+//Declara quais elementos vão ser lidos. O script vai ler todos que tiverem a classe "botao".
+const textos = document.querySelectorAll(".abra-conteudo");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-12-19t12:00:00");
+const tempoAtual = new Date;
+
+for(let i=0;i <botoes.length;i++){
+    //Variável "i" é igual a 0, mas enquanto essa variável for menor que a quantidade de elementos nessa lista, aumentamos o valor de "i", ou seja, i será a quantidade de botões.
+    botoes[i].onclick = function() {
+        for(let j=0;j<botoes.length;j++) {
+        botoes[j].classList.remove("ativo");
+        textos[j].classList.remove("ativo");
+        }
+        //Remove a classe "ativo" de todos os botões.
+        botoes[i].classList.add("ativo");
+        //Adiciona a classe ativo só no botão clicado.
+        textos[i].classList.add("ativo");
+    }
+}
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
